e8c3da24529d634a63fe68e1e6331bf1fc75c7ff
10Narratives <vanyatertyshnikov@yandex.ru>
2025-04-23 11:27:20 +0300

    Solve 11 tasks

---
+++ src/main/java/task11/Main.java
@@ -0,0 +1,25 @@
+package task11;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class Main {
+    @SuppressWarnings("CallToPrintStackTrace")
+    public static void main(String[] args) {
+        try {
+            Class<?> systemClass = Class.forName("java.lang.System");
+
+            Field outField = systemClass.getDeclaredField("out");
+            outField.setAccessible(true);
+
+            Object printStream = outField.get(null);
+
+            Class<?> printStreamClass = Class.forName("java.io.PrintStream");
+            Method printlnMethod = printStreamClass.getDeclaredMethod("println", String.class);
+
+            printlnMethod.invoke(printStream, "Hello, World");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
